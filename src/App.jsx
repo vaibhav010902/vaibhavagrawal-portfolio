486648e1305478569bb9275730e577f365f43a23
Vaibhav Agrawal <vaibhavagrawal1902@gmail.com>
2025-08-11 01:16:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,16 @@ import Footer from './component/footer/Footer';
 import Project from './component/pages/projectPage/Project';
 import Internship from './component/pages/internshipPage/Internship';
 import Contact from './component/pages/contactPage/Contact';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import { Link } from "react-router-dom";
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <>
-          <Navbar />
-          <HomePage />
-        </>
-      ),
-    },
-    {
-      path: '/project',
-      element: (
-        <>
-          <Navbar />
-          <Project />
-        </>
-      ),
-    },
-    {
-      path: '/internship',
-      element: (
-        <>
-          <Navbar />
-          <Internship />
-        </>
-      ),
-    },
-    {
-      path: '/contact',
-      element: (
-        <>
-          <Navbar />
-          <Contact />
-        </>
-      ),
-    },
-  ]);
+  
 return (
   <>
-    {/* <Navbar/>
-      <HomePage/>
-      <Project/>
-      <Internship/>
-      <Footer/> */}
-    {/* <Navbar /> */}
-    <RouterProvider router={router} />
-    {/* <Footer /> */}
+    <Navbar />
+    <Outlet/>
   </>
 )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './component/pages/homePage/HomePage';
+import Project from './component/pages/projectPage/Project';
+import Internship from './component/pages/internshipPage/Internship';
+import Contact from './component/pages/contactPage/Contact';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: '/project',
+        element: <Project/>
+      },
+      {
+        path: '/internship',
+        element: <Internship/>
+      },
+      {
+        path: '/contact',
+        element: <Contact/>
+      },
+    ]
+  }
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <App />
-      {/* <MyAnimatedComponent/> */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
